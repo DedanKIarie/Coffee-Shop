5ffdf47466a8c8b7ba5d4c1a13bd30937356a557
DedanKIarie <dedanbroo@gmail.com>
2025-05-18 19:07:19 +0300

    created coffee class with immutable name

---
+++ coffee.py
@@ -0,0 +1,10 @@
+class Coffee:
+    def __init__(self,name):
+        if isinstance(name, str) and len(name)>=3:
+            self._name = name
+        else:
+            raise ValueError("Coffee name must be at least 3 char long")
+        
+    @property
+    def name(self):
+        return self._name
